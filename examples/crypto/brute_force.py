c9b6868a83a9bf4020dd891bcaeda694f4b7641b
ptr-yudai <ptr.yudai@gmail.com>
2018-11-02 22:28:56 +0900

    Simple brute-force attack

---
+++ examples/crypto/brute_force.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from ptrlib import *
+import string
+
+known_hash = "165d3d525f6d0cbd55e42cc3058cafcc".decode("hex")
+table = string.ascii_letters
+
+m = MD5()
+password_list = brute_force_attack(3, table_len=len(table))
+for pattern in password_list:
+    password = brute_force_pattern(pattern)
+    m.update(password)
+    if m.digest() == known_hash:
+        print("Found the password!")
+        print("Password: " + password)
+        break
+    m.reset()
--- ptrlib/crypto/__init__.py
+++ ptrlib/crypto/__init__.py
@@ -1,5 +1,6 @@
 from hash.md5 import *
 from hash.sha1 import *
 from utils import *
+from brute import *
 from lenext import *
 from padcbc import *
