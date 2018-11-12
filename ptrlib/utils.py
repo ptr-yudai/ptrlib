5a2f6787584457314535d715f6835aaa4cca924e
Yudai FUJIWARA <ptr@localhost.localdomain>
2018-11-12 17:10:40 +0900

    Hex ascii converter

--- ptrlib/utils.py
+++ ptrlib/utils.py
@@ -10,3 +10,27 @@ def chunks(data, k, truncate=False):
     if truncate and len(data) % k:
         ret.pop()
     return ret
+
+def str2hex(data):
+    """Convert ascii text into hex format"""
+    return data.encode("hex")
+
+def str2int(data):
+    """Convert ascii text into integer"""
+    return int(data.encode("hex"), 16)
+
+def hex2str(data):
+    """Convert hex format into ascii text"""
+    return data.decode("hex")
+
+def hex2int(data):
+    """Convert hex format into integer"""
+    return int(data, 16)
+
+def int2str(data):
+    """Convert integer into ascii text"""
+    return hex(data)[2:].rstrip('L').decode('hex')
+
+def int2hex(data):
+    """Convert integer into hex format"""
+    return hex(data)[2:].rstrip('L')
