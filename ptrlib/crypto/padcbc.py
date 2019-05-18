1baae9915c986e271a6a3f5f504d65f21a2af4b4
theoldmoon0602 <theoldmoon0602@gmail.com>
2019-05-18 13:12:28 +0900

    padcbc: add assert when appropriate byte doesn't exist

--- ptrlib/crypto/padcbc.py
+++ ptrlib/crypto/padcbc.py
@@ -1,26 +1,31 @@
 from ptrlib.util.encoding import *
 
+
 def padding_oracle_block(decrypt, prev_block, cipher_block, bs):
-    plain = [b'\x00' for i in range(bs)]
+    plain = [b"\x00" for i in range(bs)]
 
     for i in range(bs):
-        flag = False
+        found = False
         for b in range(256):
-            dummy_block = b'\x00'*(bs - i - 1) + bytes([b])
-            for b2 in plain[bs-i:]:
-                dummy_block += bytes([b2[0] ^ (i+1) ^ prev_block[len(dummy_block)]])
+            dummy_block = b"\x00" * (bs - i - 1) + bytes([b])
+            for b2 in plain[bs - i :]:
+                dummy_block += bytes([b2[0] ^ (i + 1) ^ prev_block[len(dummy_block)]])
 
             ret = decrypt(dummy_block + cipher_block)
             if ret is True:
-                plain[bs - i - 1] = bytes([b ^ (i+1) ^ prev_block[bs - i - 1]])
+                plain[bs - i - 1] = bytes([b ^ (i + 1) ^ prev_block[bs - i - 1]])
+                found = True
                 break
             elif ret is not False:
                 raise ValueError("The function `decrypt` must return True or False")
-    return b''.join(plain)
+        assert found
+    return b"".join(plain)
 
 
 """Padding Oracle Attack on CBC encryption"""
-def padding_oracle(decrypt, cipher, bs, unknown=b'\x00', iv=None):
+
+
+def padding_oracle(decrypt, cipher, bs, unknown=b"\x00", iv=None):
     """Padding Oracle Attack
     
     Given a ciphersystem such that:
@@ -43,13 +48,14 @@ def padding_oracle(decrypt, cipher, bs, unknown=b'\x00', iv=None):
     # Split ciphertext into blocks
     cipher_blocks = []
     for i in range(0, len(cipher), bs):
-        cipher_blocks.append(cipher[i:i + bs])
+        cipher_blocks.append(cipher[i : i + bs])
     plain_blocks = [None for i in range(len(cipher_blocks))]
 
     # Break the cipher
     for k in range(len(cipher_blocks) - 1, 0, -1):
-        plain_blocks[k] = padding_oracle_block(decrypt, cipher_blocks[k-1], cipher_blocks[k], bs)
-         
+        plain_blocks[k] = padding_oracle_block(
+            decrypt, cipher_blocks[k - 1], cipher_blocks[k], bs
+        )
 
     if isinstance(unknown, str):
         unknown = str2bytes(unknown)
@@ -59,11 +65,13 @@ def padding_oracle(decrypt, cipher, bs, unknown=b'\x00', iv=None):
     else:
         plain_blocks[0] = unknown * bs
 
-    return b''.join(plain_blocks)
+    return b"".join(plain_blocks)
 
 
 """Padding Oracle Enctyption Attack on CBC encryption"""
-def padding_oracle_encrypt(decrypt, plain, bs, unknown=b'\x00'):
+
+
+def padding_oracle_encrypt(decrypt, plain, bs, unknown=b"\x00"):
     """Padding Oracle Encryption Attack
 
     Usage:
@@ -72,12 +80,14 @@ def padding_oracle_encrypt(decrypt, plain, bs, unknown=b'\x00'):
     if len(plain) % bs != 0:
         raise ValueError("The length of `plain` must be a multiple of `bs`")
 
-    cipher_blocks = [unknown*bs for _ in range(len(plain) // bs + 1)]
+    cipher_blocks = [unknown * bs for _ in range(len(plain) // bs + 1)]
 
     for k in range(len(cipher_blocks) - 1, 0, -1):
-        cipher_block = bytearray(padding_oracle_block(decrypt, cipher_blocks[k-1], cipher_blocks[k], bs))
+        cipher_block = bytearray(
+            padding_oracle_block(decrypt, cipher_blocks[k - 1], cipher_blocks[k], bs)
+        )
         for i in range(bs):
             cipher_block[i] = cipher_block[i] ^ ord(unknown) ^ plain[bs * (k - 1) + i]
-        cipher_blocks[k-1] = cipher_block
+        cipher_blocks[k - 1] = cipher_block
 
-    return b''.join(cipher_blocks)
+    return b"".join(cipher_blocks)
