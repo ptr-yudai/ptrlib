36d1e546939822aaf4a959ba1d345cb1b556a473
Yudai FUJIWARA <ptr.yudai@gmail.com>
2019-06-06 15:53:48 +0900

    Added dump for compatibility (only shows a deprecated message)

--- ptrlib/__init__.py
+++ ptrlib/__init__.py
@@ -12,15 +12,24 @@ class ColoredFormatter(Formatter):
 
         prefix = ''
         if record.levelno == INFO:
-            prefix = '{bold}{green}[+]{end}'.format(bold=Color.BOLD, green=Color.GREEN, end=Color.END)
+            prefix = '{bold}{green}[+]{end} '.format(bold=Color.BOLD, green=Color.GREEN, end=Color.END)
         if record.levelno == WARNING:
-            prefix = '{bold}{red}[+]{end}'.format(bold=Color.BOLD, red=Color.RED, end=Color.END)
+            prefix = '{bold}{red}[+]{end} '.format(bold=Color.BOLD, red=Color.RED, end=Color.END)
         elif record.levelno >= ERROR:
-            prefix = '{bold}{yellow}[WARN]{end}'.format(bold=Color.BOLD, yellow=Color.YELLOW, end=Color.END)
+            prefix = '{bold}{yellow}[WARN]{end} '.format(bold=Color.BOLD, yellow=Color.YELLOW, end=Color.END)
         else:
-            prefix = '{bold}[+]{end}'.format(bold=Color.BOLD, end=Color.END)
+            prefix = '{bold}[+]{end} '.format(bold=Color.BOLD, end=Color.END)
 
         return prefix +  super(ColoredFormatter, self).format(record)
+
+class DeprecatedLog(object):
+    def __init__(self):
+        self.warn = False
+    
+    def dump(self, msg, level=''):
+        if not self.warn:
+            logger.warning("`log.dump` is no longer available! Use `logger` instead")
+        self.warn = True
 
 # ptrlib root logger
 handler = StreamHandler()
@@ -28,3 +37,7 @@ handler.setFormatter(ColoredFormatter("%(funcName)s: %(message)s"))
 logger = getLogger(__name__)
 logger.setLevel(INFO)
 logger.addHandler(handler)
+
+# For compatibility
+log = DeprecatedLog()
+dump = log.dump
