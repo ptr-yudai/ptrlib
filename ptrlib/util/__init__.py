e113b48e85834c44fd351bf73a99f57947dc4e49
Yudai FUJIWARA <ptr.yudai@gmail.com>
2019-09-20 15:58:28 +0900

    Added chunks, flat

--- ptrlib/util/__init__.py
+++ ptrlib/util/__init__.py
@@ -2,3 +2,4 @@ from ptrlib.util.encoding import *
 from ptrlib.util.packing import *
 from ptrlib.util.logic import *
 from ptrlib.util.construct import *
+from ptrlib.util.opebinary import *
---
+++ ptrlib/util/opebinary.py
@@ -0,0 +1,34 @@
+def chunks(data, size, padding=None):
+    """Split data into chunks
+    Args:
+        data      : The target data to split
+        size (int): The size of each chunk
+        padding   : Data for padding (None if not to add padding)
+
+    Returns:
+        list: split chunks
+    """
+    assert size > 0
+    result = []
+    for i in range(0, len(data), size):
+        result.append(data[i:i+size])
+    if padding is not None and len(data) % size > 0:
+        result[-1] += padding * (size - (len(data) % size))
+    return result
+
+def flat(chunks):
+    """Concatnate chunks into a data
+    Aimed for the use of crafting ROP chains
+
+    Args:
+        chunks    : The target chunks to concat
+
+    Returns:
+        bytes: split chunks
+    """
+    assert isinstance(chunks, list)
+    result = chunks[0]
+    for i in range(1, len(chunks)):
+        result += chunks[i]
+    return result
+
