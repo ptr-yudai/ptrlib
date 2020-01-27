d4638601909e15080f591bd1f540ecd5e4a71c43
Yudai FUJIWARA <ptr.yudai@gmail.com>
2020-01-27 15:29:29 +0900

    Added crt and is_prime

--- ptrlib/crypto/number.py
+++ ptrlib/crypto/number.py
@@ -4,12 +4,18 @@ import random
 _ctx = gmpy2.get_context().precision = 4096
 
 def gen_prime(bits):
+    """ Generate a random prime """
     while True:
         n = random.getrandbits(bits)
         if gmpy2.is_prime(n):
             return n
 
+def is_prime(n):
+    """ Check if a number is prime """
+    return gmpy2.is_prime(n)
+
 def chinese_remainder_theorem(pairs):
+    """ Chinese Remainder Theorem """
     N = 1
     result = 0
     for c, n in pairs: N *= n
@@ -22,7 +28,12 @@ def chinese_remainder_theorem(pairs):
         result += c * s * m
     return result % N, N
 
+def crt(pairs):
+    # wrapper for chinese_remainder_theorem
+    return chinese_remainder_theorem(pairs)
+
 def xgcd(a, b):
+    """ Extended GCD algorithm """
     x0, x1, y0, y1 = 0, 1, 1, 0
     while a != 0:
         q, b, a = b // a, a, b % a
@@ -31,6 +42,7 @@ def xgcd(a, b):
     return b, x0, y0
 
 def inverse(a, n):
+    """ Inverse modulo """
     g, x, y = xgcd(a, n)
     if g != 1:
         logger.warn("No modular inverse")
