2ad106f56d22aac2b9f2777e3a4a4ea08fa9d6d4
Yudai FUJIWARA <ptr.yudai@gmail.com>
2021-04-15 16:53:03 +0900

    Add system call table

--- ptrlib/pwn/__init__.py
+++ ptrlib/pwn/__init__.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 from ptrlib.pwn.fsb import *
+from ptrlib.pwn.fuzz import *
 from ptrlib.pwn.sock import *
 from ptrlib.pwn.proc import *
 from ptrlib.pwn.ssh import *
 from ptrlib.pwn.robot import *
 from ptrlib.pwn.dl import *
+from ptrlib.pwn.syscall import *
