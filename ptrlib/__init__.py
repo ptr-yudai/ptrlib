58f85e30c40a4e28e907f2a5d4676e3ba34dc183
Yudai Fujiwara <ptr.yudai+dev@gmail.com>
2022-12-15 19:50:08 +0900

    Fix logger

--- ptrlib/__init__.py
+++ ptrlib/__init__.py
@@ -2,6 +2,15 @@ from .algorithm import *
 from .arch import *
 from .binary import *
 from .connection import *
+from .console import *
 from .crypto import *
 from .filestruct import *
 from .pwn import *
+from logging import getLogger, StreamHandler
+
+# ptrlib root logger
+handler = StreamHandler()
+handler.setFormatter(ColoredFormatter("%(funcName)s: %(message)s"))
+logger = getLogger(__name__)
+logger.setLevel(INFO)
+logger.addHandler(handler)
