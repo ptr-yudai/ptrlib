ptr-yudai/ptrlib

commit 30526b9e25e629ee6fa32885e3dbeb1751559467

Add debug feature

ptrlib/connection/winproc.py
@@ -95,7 +95,7 @@ class WinPipe(object):
 
         return self._recv(min(self.size, size))
 
-    def send(self, data: bytes):
+    def _send(self, data: bytes):
         """Send raw data
 
         Send raw data through the socket

ptrlib/binary/encoding/__init__.py
@@ -1,4 +1,5 @@
 from .bitconv import *
 from .byteconv import *
+from .dump import *
 from .locale import *
 from .table import *

ptrlib/binary/encoding/dump.py
@@ -1,0 +1,30 @@
+from .byteconv import str2bytes
+
+def hexdump(data, prefix='', postfix=''):
+    if isinstance(data, str):
+        data = str2bytes(data)
+    prev_display = True
+    for offset in range(0, len(data), 0x10):
+        output  = prefix
+        output += f"{offset:08x}  "
+        for i, c in enumerate(data[offset:offset+0x10]):
+            if i == 8: output += " "
+            output += f"{c:02x} "
+        if len(data[offset:]) < 0x10:
+            output += "   " * (0x10 - len(data[offset:]))
+            if len(data[offset:]) < 9:
+                output += " "
+        output += " |"
+        for c in data[offset:offset+0x10]:
+            if 0x20 <= c <= 0x7e:
+                output += chr(c)
+            else:
+                output += "."
+        output += postfix
+        if offset > 0x10 and data[offset-0x10:offset] == data[offset:offset+0x10]:
+            if prev_display:
+                print(prefix + "*" + postfix)
+                prev_display = False
+        else:
+            print(output)
+            prev_display = True

ptrlib/connection/proc.py
@@ -165,7 +165,7 @@ class UnixProcess(Tube):
         self._poll() # poll after received all data
         return data
 
-    def send(self, data: Union[str, bytes]):
+    def _send(self, data: Union[str, bytes]):
         """Send raw data
 
         Send raw data through the socket

ptrlib/connection/sock.py
@@ -87,7 +87,7 @@ class Socket(Tube):
 
         return data
 
-    def send(self, data: Union[str, bytes]):
+    def _send(self, data: Union[str, bytes]):
         """Send raw data
 
         Send raw data through the socket

ptrlib/connection/tube.py
@@ -20,6 +20,7 @@ logger = getLogger(__name__)
 class Tube(metaclass=ABCMeta):
     def __init__(self):
         self.buf = b''
+        self.debug = False
 
     @abstractmethod
     def _settimeout(self, timeout: Optional[Union[int, float]]):
@@ -70,6 +71,9 @@ class Tube(metaclass=ABCMeta):
             self._settimeout(timeout)
             data = self._recv(size, timeout=-1)
             self.buf += data
+            if self.debug:
+                logger.info(f"Received {hex(len(data))} ({len(data)}) bytes:")
+                hexdump(data, prefix="    " + Color.CYAN, postfix=Color.END)
 
         # We don't check size > len(self.buf) because Python handles it
         data, self.buf = self.buf[:size], self.buf[size:]
@@ -236,8 +240,14 @@ class Tube(metaclass=ABCMeta):
                 return group, data[:pos]
 
     @abstractmethod
-    def send(self, data: bytes):
+    def _send(self, data: bytes):
         pass
+
+    def send(self, data: bytes):
+        self._send(data)
+        if self.debug:
+            logger.info(f"Sent {hex(len(data))} ({len(data)}) bytes:")
+            hexdump(data, prefix=Color.YELLOW, postfix=Color.END)
 
     @abstractmethod
     def _socket(self) -> Optional[Any]:

ptrlib/console/color.py
@@ -15,7 +15,7 @@ class Color:
     BOLD      = '\033[1m'
     UNDERLINE = '\033[4m'
     INVISIBLE = '\033[08m'
-    REVERCE   = '\033[07m'
+    REVERSE   = '\033[07m'
 
 class ColoredFormatter(Formatter):
     def format(self, record: LogRecord):

commit f2508ae2bd9ef721395550c006987a197bb8542e

Fix WinPipe method typo, prevent WinProcess exception, and skip Linux-specific tests.
* Fix typo in method name WinPipe introduced in commit 30526b9.
* Fix ignored exception in WinProcess.__del__ when CreateProcess fails.
* Skip tests intended for the Linux platform when running on Windows.

ptrlib/connection/winproc.py
@@ -95,7 +95,7 @@ class WinPipe(object):
 
         return self._recv(min(self.size, size))
 
-    def _send(self, data: bytes):
+    def send(self, data: bytes):
         """Send raw data
 
         Send raw data through the socket
@@ -153,6 +153,7 @@ class WinProcess(Tube):
         self.stdout = WinPipe()
         self.default_timeout = timeout
         self.timeout = timeout
+        self.proc = None
 
         # Create process
         info = win32process.STARTUPINFO()

tests/connection/test_proc.py
@@ -4,10 +4,14 @@ import random
 from ptrlib import Process, is_scanf_safe
 from logging import getLogger, FATAL
 
+_is_windows = os.name == 'nt'
+
 
 class TestProcess(unittest.TestCase):
     def setUp(self):
         getLogger("ptrlib").setLevel(FATAL)
+        if _is_windows:
+            self.skipTest("This test is intended for the Linux platform")
 
     def test_basic(self):
         while True:

tests/filestruct/elf/test_elf_1.py
@@ -1,6 +1,9 @@
 import unittest
+import os
 from ptrlib.filestruct.elf import ELF
 from logging import getLogger, FATAL
+
+_is_windows = os.name == 'nt'
 
 
 PATH_ELF = "./tests/test.bin/libc-2.27.so"
@@ -9,6 +12,8 @@ BASE = 0x7fffdeadb000
 class TestELF1(unittest.TestCase):
     def setUp(self):
         getLogger("ptrlib").setLevel(FATAL)
+        if _is_windows:
+            self.skipTest("This test is intended for the Linux platform")
         self.elf = ELF(PATH_ELF)
 
     def test_symbol(self):

tests/filestruct/elf/test_elf_2.py
@@ -1,6 +1,9 @@
 import unittest
+import os
 from ptrlib.filestruct.elf import ELF
 from logging import getLogger, FATAL
+
+_is_windows = os.name == 'nt'
 
 
 PATH_ELF = "./tests/test.bin/libc-2.31.so"
@@ -9,6 +12,8 @@ BASE = 0x7fffdeadb000
 class TestELF2(unittest.TestCase):
     def setUp(self):
         getLogger("ptrlib").setLevel(FATAL)
+        if _is_windows:
+            self.skipTest("This test is intended for the Linux platform")
         self.elf = ELF(PATH_ELF)
 
     def test_symbol(self):

tests/filestruct/elf/test_elf_3.py
@@ -1,6 +1,9 @@
 import unittest
+import os
 from ptrlib.filestruct.elf import ELF
 from logging import getLogger, FATAL
+
+_is_windows = os.name == 'nt'
 
 
 PATH_ELF = "./tests/test.bin/libc-2.34.so"
@@ -9,6 +12,8 @@ BASE = 0x7fffdeadb000
 class TestELF3(unittest.TestCase):
     def setUp(self):
         getLogger("ptrlib").setLevel(FATAL)
+        if _is_windows:
+            self.skipTest("This test is intended for the Linux platform")
         self.elf = ELF(PATH_ELF)
 
     def test_symbol(self):

tests/filestruct/elf/test_elf_4.py
@@ -1,6 +1,9 @@
 import unittest
+import os
 from ptrlib.filestruct.elf import ELF
 from logging import getLogger, FATAL
+
+_is_windows = os.name == 'nt'
 
 
 PATH_ELF = "./tests/test.bin/test_echo.x86"
@@ -9,6 +12,8 @@ BASE = 0x7fdea000
 class TestELF4(unittest.TestCase):
     def setUp(self):
         getLogger("ptrlib").setLevel(FATAL)
+        if _is_windows:
+            self.skipTest("This test is intended for the Linux platform")
         self.elf = ELF(PATH_ELF)
 
     def test_plt(self):

tests/filestruct/elf/test_elf_5.py
@@ -1,6 +1,9 @@
 import unittest
+import os
 from ptrlib.filestruct.elf import ELF
 from logging import getLogger, FATAL
+
+_is_windows = os.name == 'nt'
 
 
 PATH_ELF = "./tests/test.bin/test_echo.x64"
@@ -9,6 +12,8 @@ BASE = 0x555555554000
 class TestELF5(unittest.TestCase):
     def setUp(self):
         getLogger("ptrlib").setLevel(FATAL)
+        if _is_windows:
+            self.skipTest("This test is intended for the Linux platform")
         self.elf = ELF(PATH_ELF)
 
     def test_plt(self):

tests/filestruct/elf/test_elf_6.py
@@ -1,6 +1,9 @@
 import unittest
+import os
 from ptrlib.filestruct.elf import ELF
 from logging import getLogger, FATAL
+
+_is_windows = os.name == 'nt'
 
 
 PATH_ELF = "./tests/test.bin/test_fsb.x86"
@@ -8,6 +11,8 @@ PATH_ELF = "./tests/test.bin/test_fsb.x86"
 class TestELF6(unittest.TestCase):
     def setUp(self):
         getLogger("ptrlib").setLevel(FATAL)
+        if _is_windows:
+            self.skipTest("This test is intended for the Linux platform")
         self.elf = ELF(PATH_ELF)
 
     def test_plt(self):

tests/filestruct/elf/test_elf_7.py
@@ -1,6 +1,9 @@
 import unittest
+import os
 from ptrlib.filestruct.elf import ELF
 from logging import getLogger, FATAL
+
+_is_windows = os.name == 'nt'
 
 
 PATH_ELF = "./tests/test.bin/test_fsb.x64"
@@ -8,6 +11,8 @@ PATH_ELF = "./tests/test.bin/test_fsb.x64"
 class TestELF7(unittest.TestCase):
     def setUp(self):
         getLogger("ptrlib").setLevel(FATAL)
+        if _is_windows:
+            self.skipTest("This test is intended for the Linux platform")
         self.elf = ELF(PATH_ELF)
 
     def test_plt(self):

tests/filestruct/elf/test_elf_8.py
@@ -1,6 +1,9 @@
 import unittest
+import os
 from ptrlib.filestruct.elf import ELF
 from logging import getLogger, FATAL
+
+_is_windows = os.name == 'nt'
 
 
 PATH_ELF = "./tests/test.bin/test_plt.x64"
@@ -8,6 +11,8 @@ PATH_ELF = "./tests/test.bin/test_plt.x64"
 class TestELF8(unittest.TestCase):
     def setUp(self):
         getLogger("ptrlib").setLevel(FATAL)
+        if _is_windows:
+            self.skipTest("This test is intended for the Linux platform")
         self.elf = ELF(PATH_ELF)
 
     def test_plt(self):

tests/filestruct/elf/test_elf_9.py
@@ -1,6 +1,9 @@
 import unittest
+import os
 from ptrlib.filestruct.elf import ELF
 from logging import getLogger, FATAL
+
+_is_windows = os.name == 'nt'
 
 
 PATH_ELF = "./tests/test.bin/libc-2.35.so"
@@ -9,6 +12,8 @@ BASE = 0x7fffdeadb000
 class TestELF9(unittest.TestCase):
     def setUp(self):
         getLogger("ptrlib").setLevel(FATAL)
+        if _is_windows:
+            self.skipTest("This test is intended for the Linux platform")
         self.elf = ELF(PATH_ELF)
 
     def test_symbol(self):

tests/pwn/test_fsb.py
@@ -3,10 +3,15 @@ import os
 from ptrlib import Process, fsb
 from logging import getLogger, FATAL
 
+_is_windows = os.name == 'nt'
+
 
 class TestFSB(unittest.TestCase):
     def setUp(self):
         getLogger("ptrlib").setLevel(FATAL)
+        if _is_windows:
+            # TODO: Implement test for Windows
+            self.skipTest("This test has not been implemented for Windows yet")
 
     def test_fsb32(self):
         # test 1