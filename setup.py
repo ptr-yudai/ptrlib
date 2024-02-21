c42fc73cc6e37af517ecbcf94d54c0f4d7de4c8a
Yudai Fujiwara <ptr.yudai+dev@gmail.com>
2024-02-21 16:25:02 +0900

    ptrlib 2.1.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ptrlib',
-    version='2.1.3',
+    version='2.1.4',
     description='CTF library',
     long_description=long_description,
     long_description_content_type='text/markdown',
